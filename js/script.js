73d8cf46d6d8e988a22beb881f27541ea5c6652a
enstrong <75175712+enstrong@users.noreply.github.com>
2021-01-16 18:03:41 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let phrases = [
+   { text: 'черкать карандашом экран',                 image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
+   { text: 'выйти с конфы со словами "инет плохой"',          image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
+   { text: 'нет, НЕ сидя на уроке, дайте ссылку на конфу пж', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
+   { text: 'стать патриотом школы',                           image: 'img/73фтл.jpg'},
+   { text: 'расставлять книги по полке',                      image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png'},
+   { text: 'читать новости',                                  image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png'},
+   { text: 'смотреть сериал',                                 image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png'},
+   { text: 'посмотреть что пишут в WhatsApp и Telegram',      image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png'},
+   
+];
+
+function getRandomElement(arr) {
+   let randIndex = Math.floor(Math.random() * arr.length);
+   return arr[randIndex];
+}
+
+   let button = document.querySelector('.button');
+   let phrase = document.querySelector('.phrase');
+   let advice = document.querySelector('.advice');
+   let image = document.querySelector('.image');
+
+   button.addEventListener('click', function () {
+      let randomElement = getRandomElement(phrases);
+      smoothly(phrase, 'textContent', randomElement.text)
+      smoothly(image, 'src', randomElement.image)
+
+      if (randomElement.text.length > 35) {
+         advice.style.fontSize = '33px';
+       } else {
+         advice.style.fontSize = '42px';
+       }
+
+      if (randomElement.text.length = 36) {
+         image.style.width = '60%';
+       } else {
+         image.style.width = '80%';
+       }
+   });
