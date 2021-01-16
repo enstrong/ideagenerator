3398d4424fbac476a911f1b0c0790c09f2eead6b
enstrong <75175712+enstrong@users.noreply.github.com>
2021-01-17 00:13:15 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 let phrases = [
-   { text: 'черкать карандашом экран',                 image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
-   { text: 'выйти с конфы со словами "инет плохой"',          image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
-   { text: 'нет, НЕ сидя на уроке, дайте ссылку на конфу пж', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
-   { text: 'стать патриотом школы',                           image: 'img/73фтл.jpg'},
-   { text: 'расставлять книги по полке',                      image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png'},
-   { text: 'читать новости',                                  image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png'},
-   { text: 'смотреть сериал',                                 image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png'},
-   { text: 'посмотреть что пишут в WhatsApp и Telegram',      image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png'},
-   
+   { text: 'черкать карандашом экран',                         image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
+   { text: 'вылететь с конфы со словами "инет или тимс?"',     image: 'img/tutle.png'},
+   { text: 'нет, НЕ сидя на уроке, дайте ссылку на конфу пж',  image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
+   { text: 'стать патриотом школы',                            image: 'img/73фтл.jpg'},
+   { text: 'расставлять книги по полке',                       image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png'},
+   { text: 'читать новости',                                   image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png'},
+   { text: 'смотреть сериал',                                  image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png'},
+   { text: 'посмотреть что пишут в WhatsApp и Telegram',       image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png'},
+   { text: 'рисовать в тетради',                               image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif'},
+   { text: 'стать партнёром Казахтелеком и вылететь с урока',  image: 'img.kazakhtelecom.png'},
 ];
 
 function getRandomElement(arr) {
